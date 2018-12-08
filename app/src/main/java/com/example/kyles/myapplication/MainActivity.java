34c7975cc76f1cb67ecd41daa00bbe4006f03cc7
Durstnt <klsantiago424@gmail.com>
2018-12-08 23:47:10 +0800

    added shop view

--- app/src/main/java/com/example/kyles/myapplication/MainActivity.java
+++ app/src/main/java/com/example/kyles/myapplication/MainActivity.java
@@ -12,9 +12,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button playGame = findViewById(R.id.button);
+        Button shop = findViewById(R.id.button2);
+
         playGame.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 setContentView(new GameView(MainActivity.this));
+            }
+        });
+
+        shop.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.shop);
+                Button backShop = findViewById(R.id.button4);
+                backShop.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        setContentView(R.layout.activity_main);
+                    }
+                });
             }
         });
     }
